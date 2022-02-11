6bacd667652bca8b601a31abf04a7f7ac758a64c
Bill Eger <egeriicw@gmail.com>
2022-02-11 11:37:36 -0500

    init react files

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-d3-wattenberger-tutorial
+React and D3 sandbox starting with https://wattenberger.com/blog/react-and-d3
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Circle from './components/Circle';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Circle></Circle>
+    </div>
+  );
+}
+
+export default App;
