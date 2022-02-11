8c2571d0a1bb6e2268525b5b7eed06a54f83ee78
Bill Eger <egeriicw@gmail.com>
2022-02-11 11:42:30 -0500

    Add Circle.jsx

---
+++ src/components/Circle.jsx
@@ -0,0 +1,15 @@
+import * as d3 from 'd3';
+
+const Circle = () => {
+    return (
+        <svg>
+            <circle
+              cx="150"
+              cy="77"
+              r="40"
+            />
+        </svg>
+    )
+}
+
+export default Circle;
